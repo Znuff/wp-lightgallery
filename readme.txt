b6f1088e86dc512cee5be5ef35c8baca9f82ff1a
Znuff <Znuff@users.noreply.github.com>
2025-12-14 04:03:05 +0200

    Initial files

---
+++ readme.txt
@@ -0,0 +1,5 @@
+=== Plugin Name ===
+Contributors: znuff
+Tags: lightbox, lightgallery
+Requires at least: 6.0
+Tested up to: 6.9
---
+++ wp-lightgallery.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: wp-lightgallery
+ * Plugin URI: https://linge-ma.ro/wp-lightgallery
+ * Description: lightGallery for Wordpress
+ * Version: r1
+ * Author: Znuff
+ * Author URI: https://linge-ma.ro
+ * License: GPLv3
+ */
+
+
